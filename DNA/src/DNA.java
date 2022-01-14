624420387dbab44b2b68044767fedc047f266705
TeslaSTEMCSTeacher <bkankelborg@lwsd.org>
2022-01-14 08:33:59 -0800

    Populating starter project: Part 2

---
+++ DNA/src/DNA.java
@@ -0,0 +1,45 @@
+
+/*
+ * Name
+ * Date
+ * APCS Period
+ * DNA Project
+ * 
+ * Program Description
+ */
+import java.io.FileNotFoundException;
+
+public class DNA
+{
+    /* 
+     * Your main should call startAnalysis or startAnalysisPart2 with the name of your current
+     * test file. You may prompt the user for the filename if you wish for
+     * more flexible testing. NOTE: I will not be calling your main method from my test cases at all!
+     */
+    public static void main(String[] args) throws FileNotFoundException
+    {
+        
+    }
+    
+    /*
+     * This method should serve as the primary method for Part 1 of this project.
+     * I call this method directly from my test cases for Part 1 and you therefore should
+     * NOT modify this header at all.
+     */
+    public static void startAnalysis(String filename) throws FileNotFoundException
+    {
+        
+    }
+    
+    /*
+     * This method should serve as the primary method for Part 2 of this project.
+     * I call this method directly from my test cases for Part 2 and you therefore should
+     * NOT modify this header at all. You will need to replace null with a proper return value
+     * to pass all test cases.
+     */
+    public static String[][] startAnalysisPart2(String filename) throws FileNotFoundException
+    {
+        return null;
+    }
+    
+}
